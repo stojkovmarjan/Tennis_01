8497245481bf644f191135edcfb50e31e37f0247
Maryan Stoykov <stojkovmarjan@gmail.com>
2017-08-16 23:10:20 +0200

    Add project files.

---
+++ Tennis_01/TenisGame.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tennis_01
+{
+    class TenisGame
+    {
+        private TenisPlayer tenisPlayer1;
+        private TenisPlayer tenisPlayer2;
+
+        // constructors
+        public TenisGame()
+        {
+            tenisPlayer1 = new TenisPlayer("Player1");
+            tenisPlayer2 = new TenisPlayer("Player2");
+        }
+        public TenisGame(TenisPlayer player1,TenisPlayer player2)
+        {
+            tenisPlayer1 = player1;
+            tenisPlayer2 = player2;
+        }
+        public TenisGame(string player1Name, string player2Name)
+        {
+            tenisPlayer1 = new TenisPlayer(player1Name);
+            tenisPlayer2 = new TenisPlayer(player2Name);
+        }
+        //----------------------------------------------------------
+    }
+}
---
+++ Tennis_01/TenisPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tennis_01
+{
+    class TenisPlayer
+    {
+        private string playerName;
+        private int score;
+        private bool gameService;
+
+        public TenisPlayer()
+        {
+            playerName = "";
+        }
+
+        public TenisPlayer(string name)
+        {
+            playerName = name;
+        }
+    }
+}
